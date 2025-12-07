bacd0a69609e836977eb5f97672089bdb81077e3
Sanjeev Singh <sanjeev.2311.singh@gmail.com>
2025-12-07 18:07:47 +0530

    day 7 done
    Date:      Sun Dec 7 18:07:47 2025 +0530

--- D7/P7.hs
+++ D7/P7.hs
@@ -18,13 +18,29 @@ solve xs (m, n) (i, j) s
 		(b, so) = solve xs (m, n) (i, j + 1) s''
 
 solve' :: [String] -> (Int, Int) -> Coords -> M.Map Coords Integer -> (Integer, M.Map Coords Integer)
-solve' xs (m, n) (i, j) mp = undefined -- Todo : Did manage to do in Python but need to figure out how to do a BFS here
+solve' xs (m, n) (i, j) mp
+	| or [i < 0, i >= m, j < 0, j >= n] = (0, mp)
+	| Just v <- M.lookup (i, j) mp = (v, mp)
+	| curr == '.' = (dotRightV + dotLeftV + dotUpV, M.insert (i, j) (dotRightV + dotLeftV + dotUpV) dotUpM)
+	| curr == '^' = (splitUpV, M.insert (i, j) (splitUpV) splitUpM)
+	where
+		curr = xs !! i !! j
+		(dotRightV, dotRightM) = if and [ j + 1 < n, xs !! i !! (j + 1) == '^' ] then (solve' xs (m, n) (i, j + 1) mp) else (0, mp)
 
+		(dotLeftV, dotLeftM) = if and [ j - 1 >= 0, xs !! i !! (j - 1) == '^' ] then (solve' xs (m, n) (i, j - 1) dotRightM) else (0, dotRightM)
+
+		(dotUpV, dotUpM) = if and [ i - 1 >= 0, let p = xs !! (i - 1) !! j in or [p == '.', p == 'S'] ]
+																				then (solve' xs (m, n) (i - 1, j) dotLeftM) else (0, dotLeftM)
+		(splitUpV, splitUpM) = if and [ i - 1 >= 0, let p = xs !! (i - 1) !! j in or [p == '.', p == 'S'] ] then (solve' xs (m, n) (i - 1, j) mp) else (0, mp)
 
 main = do
 	inp <- fmap (words) $ readFile "input.txt"
+	(m, n) <- pure $ (length inp, length (inp !! 0))
 	ystart <- pure $ length $ takeWhile (/= 'S') (head inp)
-	print $ fst $ solve inp (length inp, length (inp !! 0)) (0, ystart) S.empty
-	-- mp <- pure  $ solve' inp (length inp, length (inp !! 0)) (0, ystart) $ M.singleton (0, ystart) 1
-	-- print $ fst mp
-	-- print $ filter (\((a, b), c) -> a == (length inp - 1)) $ M.toList (snd mp)
+	print $ fst $ solve inp (m, n) (0, ystart) S.empty
+
+	print $ foldr (\(a, b) c -> b + c) 0 $ 
+				filter (\((a, b), c) -> a == (m - 1)) $ 
+				M.toList $ snd $ 
+				foldr (\c (v, acc) -> solve' inp (m, n) c acc) (0, M.singleton (0, ystart) 1) [(i, j) | i <- [0..(m - 1)], j <- [0..(n - 1)]]
+
